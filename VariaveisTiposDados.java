1b24c48f868891f2d8f913bc46ee3988ab281346
renanrcs <renan.cavalcante@yahoo.com>
2022-06-21 13:47:47 -0300

    Todas as variaveis declaradas de forma errada estão como comentario

---
+++ VariaveisTiposDados.java
@@ -0,0 +1,51 @@
+
+public class VariaveisTiposDados {
+
+	public static void main(String[] args) {
+		
+		int i;
+		//int i;
+		int I;
+		//int 1a;
+		int _1a;
+		int $aq;
+		
+		i = 5;
+		I = 10;
+		_1a = 20;
+		$aq = 7;
+		
+		final int j = 10;
+		//j = 15;
+		int asrn24678md;
+		int asrn246_78md;
+		int asrc2$784_df = 10;
+		//int aksr2$78%44_kf = 55;
+		
+		asrn24678md = 50;
+		asrn246_78md = 80;
+		
+		int quantidadeProduto = 60;
+		int QuantidadeProduto;
+		final int NUMERO_TENTATIVAS = 8;
+		final int numero_tentativas = 9;
+		int QUANTIDADE_OPCOES = 38;
+		int qtdProd;
+		
+		System.out.println(i);
+		System.out.println(I);
+		System.out.println(_1a);
+		System.out.println($aq);
+		
+		System.out.println(j);
+		System.out.println(asrn24678md);
+		System.out.println(asrn246_78md);
+		
+		System.out.println(quantidadeProduto);
+		System.out.println(NUMERO_TENTATIVAS);
+		System.out.println(QUANTIDADE_OPCOES);
+		
+
+	}
+
+}
