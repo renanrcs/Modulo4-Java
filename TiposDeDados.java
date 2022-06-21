aa5f23844831e9d7037d66c42ad228069e517aa4
renanrcs <renan.cavalcante@yahoo.com>
2022-06-21 18:09:18 -0300

    Classe de Ex para tipo de dados

---
+++ TiposDeDados.java
@@ -0,0 +1,56 @@
+
+public class TiposDeDados {
+
+	public static void main(String[] args) {
+		
+		byte b1 = 10;
+		byte b2 = 20;
+		
+		short s1 = 20000;
+		//short s2 = 40000;
+		
+		//int i1 = -10000000000;
+		int i2 = 28500;
+		
+		long l1 = 1000000000000000000L;
+		long l2 = 2004005000500055000L;
+		
+		//float f1 = 4.5;
+		float f2 = 10.68f;//O comum é usar Maiuscula
+		
+		double d1 = 85.69;
+		double d2 = 99.04d;
+		
+		char c1 = 'W';
+		//char c2 = 'Tw';
+		char c3 = '\u0057';
+		
+		String st1 = "Fulano";
+		String st2 = "cicrano";
+		String st3 = "dfjiru ciugreig 89fff regerg";
+		
+		//String dt1 = "21/06/2022";
+		
+		boolean bo1 = true;
+		boolean bo2 = false;
+		
+		System.out.println(b1);
+		System.out.println(b2);
+		System.out.println(s1);
+		System.out.println(i2);
+		System.out.println(b1);
+		System.out.println(l1);
+		System.out.println(l2);
+		System.out.println(f2);
+		System.out.println(d1);
+		System.out.println(d2);
+		System.out.println(c1);
+		System.out.println(c3);
+		System.out.println(st1);
+		System.out.println(st2);
+		System.out.println(st3);
+		System.out.println(bo1);
+		System.out.println(bo2);
+	}
+
+}
--- Variaveis.java
+++ Variaveis.java
@@ -1,5 +1,5 @@
 
-public class VariaveisTiposDados {
+public class Variaveis {
 
 	public static void main(String[] args) {
 		
